bc93c7a9501f7091af57df49dc56358b291cddfc
Mike <mikelrap@gmail.com>
2021-06-29 16:52:06 +0300

    index.d

---
+++ dist/src/example/network.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.networkIdentifier = void 0;
+const __1 = require("../../");
+exports.networkIdentifier = new __1.Client.NetworkIdentifier('Rosetta', 'Testnet');
